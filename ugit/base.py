39dd953254c01dc57e255e9123f274b96ff1d295
Vadym <vadymkravtcsov@gmail.com>
2026-01-02 21:05:05 +0200

    feat: added a function that recursively outputs all files in the current directory

--- ugit/base.py
+++ ugit/base.py
@@ -1,1 +1,11 @@
 from . import data
+from pathlib import Path
+
+def write_tree(directory='.'):
+	for entry in Path(directory).iterdir():
+
+		if entry.is_file() and not entry.is_symlink():
+			print(entry)
+
+		elif entry.is_dir() and not entry.is_symlink():
+			write_tree(entry)
